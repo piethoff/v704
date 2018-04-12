17a5e711e2dfb81c4cfe7347d16f6286fd471190
Jan Jaekel <jan.jaekel@tu-dortmund.de>
2018-04-12 15:32:23 +0200

    added durchfuehrung

--- plot.py
+++ plot.py
@@ -1,16 +1,1 @@
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import numpy as np
-from mathe import matheplot as mp
 
-mpl.use('pgf')
-mpl.rcParams.update({
-    'pgf.preamble': r'\usepackage{siunitx}',
-})
-
-x = np.linspace(0, 10, 20)
-y = x ** np.sin(x)
-
-mp(x,y, dpi=300, factor=160)
-
-plt.savefig("1.pdf", transparent=True)
